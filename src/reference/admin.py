d1fc6f73db784ceaeb984d0a76b237db9fd4dc87
Andrey_Komar <andru_85@tut.by>
2023-05-21 16:59:37 +0300

    add four reference

--- src/reference/admin.py
+++ src/reference/admin.py
@@ -5,3 +5,6 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Genres)
+admin.site.register(models.Authors)
+admin.site.register(models.Publish)
+admin.site.register(models.Series)
--- src/reference/models.py
+++ src/reference/models.py
@@ -5,6 +5,26 @@ from django.db import models
 class Genres(models.Model):
     name = models.CharField(verbose_name='Genre name', max_length=50)
     description = models.TextField(verbose_name='Genre description', null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class Authors(models.Model):
+    name = models.CharField(verbose_name='Author name', max_length=30)
+    description = models.TextField(verbose_name='Author description', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Series(models.Model):
+    name = models.BigIntegerField(verbose_name='Series book')
+    
+    def __str__(self):
+        return self.name
+    
+class Publish(models.Model):
+    name = models.CharField(verbose_name='Publishing office', max_length=50)
+    description = models.TextField(verbose_name='Publishing office description', null=True, blank=True)
 
     def __str__(self):
         return self.name

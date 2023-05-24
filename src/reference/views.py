7cd79a140902841f4284dbc57fa71d7ab599c02f
Andrey_Komar <andru_85@tut.by>
2023-05-25 00:58:04 +0300

    add index.html

--- src/reference/views.py
+++ src/reference/views.py
@@ -4,11 +4,16 @@ from random import randint
 from . import models
 
 # Create your views here.
-def home_page(request):
-    return HttpResponse('reference')
+
+# def home_page(request):
+#     return HttpResponse('reference')
 
 #test Read:
 def view_refer_genre(request, pk):
     genr = models.Genre.objects.get(pk=int(pk))
     html = f"Genre - {genr.pk} : {genr.name}"
     return HttpResponse(html)  
+
+# test 2 
+def home_page(request):
+    return render(request, template_name='reference/index.html', context={'key': models.Genre.objects.get(pk=1)})

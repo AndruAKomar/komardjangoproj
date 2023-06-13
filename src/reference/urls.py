130106e6a334d0a946c6a56d7dc1be9021153859
Andrey_Komar <andru_85@tut.by>
2023-06-13 22:37:27 +0300

    rename view Genre in Reference

--- src/reference/urls.py
+++ src/reference/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name='reference'
 urlpatterns = [
     path('genre/<int:pk>', views.GenreView.as_view(), name='GenreView'),   #переход к конкретному объекту/деталям Жанра по его РК
-    path('genre-list/', views.ReferenceListView.as_view(), name='GenreListView'),     #переход к просмотру всех объектов Жанра
+    path('reference-list/', views.ReferenceListView.as_view(), name='ReferenceListView'),     #переход к просмотру всех объектов Жанра
     path('genre-add/', views.GenreCreateView.as_view(), name='GenreCreateView'),        #переход к форме для добавления нового Жанра
     path('genre-upd/<int:pk>', views.GenreUpdateView.as_view(), name='GenreUpdateView'),        #переход к форме для редактирования Жанра по его РК
     path('genre-delete/<int:pk>', views.GenreDeleteView.as_view(), name='GenreDeleteView'),     #переход к форме для удаления Жанра по его РК

884ccaed23b984e1fea3d506f5aea5ac8d277fa2
Andrey_Komar <andru_85@tut.by>
2023-07-03 15:03:21 +0300

    add order

--- src/orders/views.py
+++ src/orders/views.py
@@ -20,42 +20,7 @@ class CartDetailView(DetailView):
             cont["total_quantity_in_cart"] = Cart.objects.get(pk=cart_pk).total_quantity
         return cont
 
-    def get_object(self, *args, **kwargs):
-        # cart_id
-        cart_pk = self.request.session.get("cart_id") # int / None
-        customer = self.request.user # User / AnonimousUser -> User / None
-        if customer.is_anonymous:
-            customer = None
-        cart, created = Cart.objects.get_or_create(
-            pk=cart_pk,
-            defaults={
-                "customer": customer
-            }
-        )
-        # cart
-        good_id = self.request.GET.get("good_id")
-        quantity = self.request.GET.get("quantity")
-        if good_id and quantity:
-            quantity = int(quantity)
-            good = Book.objects.get(pk=int(good_id))
-            price = Book.objects.get(pk=int(good_id)).price
-            good_in_cart, good_in_cart_created = BookInCart.objects.get_or_create(
-                cart=cart,
-                good=good,
-                defaults={
-                    "quantity":quantity,
-                    "price": price * quantity
-                }
-            )
-            if not good_in_cart_created:
-                good_in_cart.quantity = good_in_cart.quantity + quantity
-                good_in_cart.price = good_in_cart.price + price*quantity
-                good_in_cart.save()
-            if created:
-                self.request.session['cart_id'] = cart.pk
-        return cart
-
-
+    
 class CartAddDeleteItemView(DetailView):
     template_name = "orders/cart.html"
     model = Cart
@@ -91,20 +56,3 @@ class CartAddDeleteItemView(DetailView):
             good_in_cart.price = good_in_cart.quantity * price
             good_in_cart.save()
         return cart
-
-
-
-
-# <script>
-#       function addGood(pk) {
-#         console.log("add" + pk)
-#         redirect("add", pk)
-#       }
-#       function deleteGood(pk) {
-#         console.log("delete" + pk)
-#         redirect("delete", pk)
-#       }
-#       function redirect(action, pk) {
-#         window.location.href = "{% url "orders:cart-items-edit" %}" + '?action=${action}&good=' + pk;
-#       }
-#   </script>
